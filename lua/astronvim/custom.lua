cd9155145771c13570f0c463fea216fce7393a8d
Keivan-sf <keyvan0082@gmail.com>
2024-02-10 11:05:05 +0330

    Add harpoon

--- lua/astronvim/custom.lua
+++ lua/astronvim/custom.lua
@@ -1,6 +1,6 @@
 -- copy to clipboard
-vim.keymap.set({ "n", "v" }, "y", "y")
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set({"n", "v"}, "y", "y")
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
 -- move stuff around in visual mode
@@ -10,3 +10,18 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.lsp.handlers["textDocument/publishDiagnostics"] =
     vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics,
                  {virtual_text = false})
+
+-- harpoon
+local harpoon = require("harpoon")
+vim.keymap.set("n", "<leader>add", function() harpoon:list():append() end)
+vim.keymap.set("n", "<C-e>",
+               function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+vim.keymap.set("n", "<leader>aj", function() harpoon:list():select(1) end)
+vim.keymap.set("n", "<leader>aa", function() harpoon:list():select(2) end)
+vim.keymap.set("n", "<leader>as", function() harpoon:list():select(3) end)
+vim.keymap.set("n", "<leader>ad", function() harpoon:list():select(4) end)
+
+-- Toggle previous & next buffers stored within Harpoon list
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
